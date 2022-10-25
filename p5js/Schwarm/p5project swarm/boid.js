239f655c3194df9fd89422510ff4deb1ca452653
Melanie Gmünder <melanie.gmuender@stud.hslu.ch>
2022-10-25 20:09:27 +0200

    p5js flocking added

---
+++ p5js/Schwarm/p5project swarm/boid.js
@@ -0,0 +1,136 @@
+class Boid {
+  constructor() {
+    this.position = createVector(random(width), random(height));
+    this.velocity = p5.Vector.random2D();
+    this.velocity.setMag(random(2, 4));
+    this.acceleration = createVector();
+    this.maxForce = initalmaxForce; //Controls, how fast they align.
+    this.maxSpeed = initalSpeed;
+    this.boidsView = initalBoidsView; //perseption
+  }
+
+  //donut world
+  edges(){
+    if(this.position.x > width) {
+        this.position.x = 0;
+    } else if(this.position.x < 0) {
+        this.position.x = width;
+    }
+    if(this.position.y > width) {
+        this.position.y = 0;
+    } else if(this.position.y < 0) {
+        this.position.y = width;
+    }
+  }
+
+  align(boids) {
+    let steering = createVector(); //Vector (force) to correct the direction
+    let total = 0;
+    for (let other of boids) {
+      let distance = dist(
+        this.position.x,
+        this.position.y,
+        other.position.x,
+        other.position.y,
+      );
+
+      //Just look at boids in view
+      if (other != this && distance <= this.boidsView) {
+        steering.add(other.velocity);
+        total++;
+      }
+    }
+    if (total > 0) {
+      steering.div(total);
+      steering.setMag(this.maxSpeed);
+      steering.sub(this.velocity);
+      steering.limit(this.maxForce); //Limits magnitude
+    }
+    return steering;
+  }
+
+  cohesion(boids) {
+    let steering = createVector();
+    let total = 0;
+    for (let other of boids) {
+      let distance = dist(
+        this.position.x,
+        this.position.y,
+        other.position.x,
+        other.position.y,
+      );
+
+      if (other != this && distance <= this.boidsView) {
+        steering.add(other.position);
+        total++;
+      }
+    }
+    if (total > 0) {
+      steering.div(total);
+      steering.sub(this.position);
+      steering.setMag(this.maxSpeed);
+      steering.sub(this.velocity);
+      steering.limit(this.maxForce);
+    }
+    return steering;
+  }
+
+  separation(boids) {
+    let steering = createVector();
+    let total = 0;
+    for (let other of boids) {
+      let distance = dist(
+        this.position.x,
+        this.position.y,
+        other.position.x,
+        other.position.y,
+      );
+
+      if (other != this && distance <= this.boidsView*2) {
+        let diff = p5.Vector.sub(this.position, other.position);
+        diff.div(distance * distance);
+        steering.add(diff);
+        total++;
+      }
+    } 
+    if (total > 0) {
+      steering.div(total);
+      steering.setMag(this.maxSpeed);
+      steering.sub(this.velocity);
+      steering.limit(this.maxForce);
+    }
+    return steering;
+  }
+
+  //Apply rules
+  flock(boids) {
+    let alignment = this.align(boids);
+    let cohesion = this.cohesion(boids);
+    let separation = this.separation(boids);
+    
+    this.acceleration.add(alignment);
+    this.acceleration.add(cohesion);
+    this.acceleration.add(separation);
+
+    //Update with slider's value
+    this.maxForce = maxForceSlider.value();
+    this.maxSpeed = maxSpeedSlider.value();
+    this.boidsView = boidsViewSlider.value();
+    alignment.mult(alignSlider.value());
+    cohesion.mult(cohesionSlider.value());
+    separation.mult(separationSlider.value());
+  }
+
+  updateBoids() {
+    this.position.add(this.velocity);
+    this.velocity.add(this.acceleration);
+    this.velocity.limit(this.maxSpeed);
+    this.acceleration.mult(0); //Reset vector
+  }
+
+  showBoids() {
+    strokeWeight(8);
+    stroke(255);
+    point(this.position.x, this.position.y);
+  }
+}
---
+++ p5js/Schwarm/p5project swarm/sketch.js
@@ -0,0 +1,58 @@
+const flock = [];
+const initalmaxForce = 1;
+const initalSpeed = 4;
+const initalBoidsView = 50;
+
+let maxForceSlider,
+  maxSpeedSlider,
+  boidsViewSlider,
+  alignSlider,
+  cohesionSlider,
+  separationSlider;
+  
+let font;
+
+function preload() {
+  font = loadFont("ebrima.ttf");
+}
+
+function setup() {
+  createCanvas(1000, 600);
+  maxForceSlider = createSlider(0.5, 1.5, initalmaxForce, 0.01);
+  maxForceSlider.position(5, 40);
+  maxSpeedSlider = createSlider(1, 8, initalSpeed, 0.05);
+  maxSpeedSlider.position(5, 70);
+  boidsViewSlider = createSlider(10, 100, initalBoidsView, 0.05);
+  boidsViewSlider.position(5, 100);
+  alignSlider = createSlider(0, 2, 1, 0.1);
+  alignSlider.position(5, 130);
+  cohesionSlider = createSlider(0, 2, 1, 0.1);
+  cohesionSlider.position(5, 160);
+  separationSlider = createSlider(0, 2, 1, 0.1);
+  separationSlider.position(5, 190);
+  textFont(font, alignSlider.height + 5);
+
+  for (let i = 0; i < 200; i++) {
+    flock.push(new Boid());
+  }
+}
+
+function draw() {
+  background(51);
+
+  for (let boid of flock) {
+    boid.edges();
+    boid.flock(flock);
+    boid.updateBoids();
+    boid.showBoids();
+  }
+
+  noStroke();
+  fill("yellow");
+  text("max force", 15 + alignSlider.width, 55);
+  text("max speed", 15 + alignSlider.width, 85);
+  text("boid's view", 15 + alignSlider.width, 115);
+  text("align", 15 + alignSlider.width, 145);
+  text("cohesion", 15 + alignSlider.width, 175);
+  text("separation", 15 + alignSlider.width, 205);
+}
