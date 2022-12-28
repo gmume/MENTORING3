b8aea1d50710b88eb2679c3433e6a38d82af4628
Melanie Gmünder <melanie.gmuender@stud.hslu.ch>
2022-12-28 13:50:19 +0100

    Added Flocking p5.js

---
+++ content/1 Flocking p5.js/sketch.js
@@ -0,0 +1,55 @@
+const flock = [];
+
+let maxForceSlider,
+  maxSpeedSlider,
+  boidsViewSlider,
+  alignSlider,
+  cohesionSlider,
+  separationSlider;
+  
+let font;
+
+function preload() {
+  font = loadFont("ebrima.ttf");
+}
+
+function setup() {
+  createCanvas(1000, 600);
+  maxForceSlider = createSlider(1, 5, 1, 0.05);
+  maxForceSlider.position(5, 40);
+  maxSpeedSlider = createSlider(1, 5, 4, 0.05);
+  maxSpeedSlider.position(5, 70);
+  boidsViewSlider = createSlider(1, 50, 50, 0.05);
+  boidsViewSlider.position(5, 100);
+  alignSlider = createSlider(0, 5, 1, 0.05);
+  alignSlider.position(5, 130);
+  cohesionSlider = createSlider(0, 5, 1, 0.05);
+  cohesionSlider.position(5, 160);
+  separationSlider = createSlider(0, 5, 1, 0.05);
+  separationSlider.position(5, 190);
+  textFont(font, alignSlider.height + 5);
+
+  for (let i = 0; i < 100; i++) {
+    flock.push(new Boid());
+  }
+}
+
+function draw() {
+  background(51);
+
+  for (let boid of flock) {
+    boid.edges();
+    boid.update();
+    boid.flock(flock);
+    boid.show();
+  }
+
+  noStroke();
+  fill("yellow");
+  text("max force", 15 + alignSlider.width, 55);
+  text("max speed", 15 + alignSlider.width, 85);
+  text("boid's view", 15 + alignSlider.width, 115);
+  text("align", 15 + alignSlider.width, 145);
+  text("cohesion", 15 + alignSlider.width, 175);
+  text("separation", 15 + alignSlider.width, 205);
+}
