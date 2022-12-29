722dea228f84c3171a7d82a86b20a278ae824eeb
Melanie Gmünder <melanie.gmuender@stud.hslu.ch>
2022-12-29 11:23:56 +0100

    Added content scriptable object

--- MENTORING3/Assets/FLOCK2/Scripts/FlockBehavior.cs
+++ MENTORING3/Assets/FLOCK2/Scripts/FlockBehavior.cs
@@ -5,3 +5,7 @@ public abstract class FlockBehavior : ScriptableObject
 {
     public abstract Vector3 CalculateMove(Boid boid, List<Transform> context, Flock flock); //context means other Boids or obstacles
 }
+
+
+
+
